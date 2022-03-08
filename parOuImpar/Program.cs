1efe1ace4a18c52919edd74f16fe358deeb3e79d
Gustavo Henrique Carl Dos Santos <gustavo_carl@live.com>
2022-03-07 21:42:24 -0300

    Adicionar arquivos de projeto.

---
+++ parOuImpar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace parOuImpar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int primeiroNumero, segundoNumero, soma;
+            Console.WriteLine("Por favor, informe um número! ");
+            primeiroNumero = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Por favor, insira o segundo número: ");
+            segundoNumero = Convert.ToInt32(Console.ReadLine());
+            soma = primeiroNumero + segundoNumero;
+            if (soma % 2 == 0)
+                Console.WriteLine("O número {0} é par!", soma);
+            else
+                Console.WriteLine("O número {0} é impar!", soma);
+            Console.ReadKey();
+        }
+    }
+}
